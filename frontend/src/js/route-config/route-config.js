b1f1a32c3c1a9502b578a8b9a7876b26a8b12774
Ayukha <ayussh.khare@gmail.com>
2020-03-04 19:15:32 +0530

    Fix #2698:Frontend added

--- frontend/src/js/route-config/route-config.js
+++ frontend/src/js/route-config/route-config.js
@@ -137,6 +137,17 @@
             authenticate: true
         };
 
+        var partcipated_challenges = {
+            name: "web.participated-challenge",
+            parent: "web",
+            url: "/participated-challenges",
+            templateUrl: baseUrl + "/web/participated-challenges.html",
+            controller: 'ParticipatedChallengesCtrl',
+            controllerAs: 'participatedChallenges',
+            title: 'Participated Challenges',
+            authenticate: true
+        };
+
         var host_analytics = {
             name: "web.host-analytics",
             parent: "web",
@@ -469,6 +480,8 @@
         $stateProvider.state(host_analytics);
         $stateProvider.state(teams);
         $stateProvider.state(hosted_challenges);
+        $stateProvider.state(partcipated_challenges);
+
 
         // challenge host teams
         $stateProvider.state(challenge_host_teams);
